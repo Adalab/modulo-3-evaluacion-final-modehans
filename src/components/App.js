d734c6b5b6efff291b08fc3ce00784e159298160
modehans <monicaglezsanch@gmail.com>
2022-08-27 16:03:40 +0200

    Create call to Api function and filtre data

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,31 @@
 import '../styles/App.scss';
+import { useEffect, useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Header from './Header';
+import callToApi from '../services/callToApi';
 const App = () => {
-  return (
-    <>
-      <Header />
+  const [dataCharacters, setDataCharacters] = useState([]);
 
+  useEffect(() => {
+    callToApi().then((dataApi) => {
+      setDataCharacters(dataApi);
+    });
+  }, []);
+
+  return (
+    <div className="container">
+      <Header />
       <Routes>
         <Route
           path="/"
           element={
-            <main className="container">
+            <main>
               <section>Filters</section>
               <ul>
                 Character List
-                <Link to="/characterDetail">
-                  {' '}
-                  <li>Character Card</li>
-                </Link>
+                <li>
+                  <Link to="/characterDetail">Character Card </Link>
+                </li>
               </ul>
             </main>
           }
@@ -26,14 +34,14 @@ const App = () => {
         <Route
           path="/characterDetail"
           element={
-            <main className="container">
+            <main>
               <Link to="/">Volver</Link>
               <div>Character Detail </div>
             </main>
           }
         />
       </Routes>
-    </>
+    </div>
   );
 };
 
---
+++ src/services/callToApi.js
@@ -0,0 +1,21 @@
+const callToApi = () => {
+  return fetch(`http://hp-api.herokuapp.com/api/characters/`)
+    .then((response) => response.json())
+    .then((response) => {
+      const data = response.map((item, index) => {
+        let newImage = `https://via.placeholder.com/256x256/816f9f/000000/?text=${item.name}`;
+        return {
+          id: index,
+          image: item.image === '' ? newImage : item.image,
+          name: item.name,
+          alive: item.alive,
+          gender: item.gender,
+          species: item.species,
+          alternate_names: item.alternate_names,
+        };
+      });
+      return data;
+    });
+};
+
+export default callToApi;
