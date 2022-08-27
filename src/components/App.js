c3fe85a4236925ad62043c75e48ed529b257a5da
modehans <monicaglezsanch@gmail.com>
2022-08-27 11:53:57 +0200

    Create route structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,38 @@
 import '../styles/App.scss';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Header from './Header';
 const App = () => {
   return (
     <>
       <Header />
 
-      <main>
-        <section> Filter</section>
-        <ul>
-          Character List
-          <li>Character Card</li>
-        </ul>
-      </main>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <main className="container">
+              <section>Filters</section>
+              <ul>
+                Character List
+                <Link to="/characterDetail">
+                  {' '}
+                  <li>Character Card</li>
+                </Link>
+              </ul>
+            </main>
+          }
+        />
+
+        <Route
+          path="/characterDetail"
+          element={
+            <main className="container">
+              <Link to="/">Volver</Link>
+              <div>Character Detail </div>
+            </main>
+          }
+        />
+      </Routes>
     </>
   );
 };
