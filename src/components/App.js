78e8698330f4a06c7c7a6d71561134a4a1a7f806
modehans <monicaglezsanch@gmail.com>
2022-08-27 08:49:42 +0200

    Initial html structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import '../styles/layout/App.scss';
 import { Routes, Route } from 'react-router-dom';
 const App = () => {
-  return <div>Hola mundo</div>;
+  return (
+    <>
+      <header>
+        <h1>Harry Potter</h1>
+      </header>
+      <main>
+        <section> Filter</section>
+        <ul>
+          {' '}
+          Character List
+          <li>Character Card</li>
+        </ul>
+      </main>
+    </>
+  );
 };
 
 export default App;
