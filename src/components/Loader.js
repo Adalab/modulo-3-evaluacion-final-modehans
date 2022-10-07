f763ce6a40fa3cf11a46c483fb2f30b76d8e2814
modehans <monicaglezsanch@gmail.com>
2022-10-07 11:52:38 +0200

    Clean loader code

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -9,6 +9,7 @@ const Loader = () => {
           <div className="scar s2"></div>
         </div>
         <div className="glasses-left"></div>
+        <div className="glasses-middle"></div>
         <div className="glasses-right"></div>
       </div>
     </div>
